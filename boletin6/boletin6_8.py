a4ca381ab0ff987a758b90b64d7d069d162453f3
Gabriela <gmartinezmenegatty@danielcastelao.org>
2025-10-02 22:50:28 +0200

    feat: :sparkles: boletin6_8

---
+++ boletin6/boletin6_8.py
@@ -0,0 +1,7 @@
+precios = [50, 75, 46, 22, 80, 65, 8]
+
+preciomin = min(precios)
+preciomax = max(precios)
+
+print(f"El precio mas bajo es: {preciomin}")
+print(f"El precio mas alto es: {preciomax}")
