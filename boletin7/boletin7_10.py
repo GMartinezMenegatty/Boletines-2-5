7fa311d5411893e1cbc793dba1b500bf2929618b
Gabriela <gmartinezmenegatty@danielcastelao.org>
2025-10-07 22:27:11 +0200

    feat: :sparkles: boletin7_10

---
+++ boletin7/boletin7_10.py
@@ -0,0 +1,10 @@
+texto = "Ola, son alumno de DAM1, e son programador desde o 2025"
+
+espacios = texto.count(" ")
+print(espacios)
+
+digitos = sum(texto.count(d) for d in "0123456789")
+print(digitos)
+
+letras = sum(1 for c in texto if c.isalpha())
+print(letras)   
