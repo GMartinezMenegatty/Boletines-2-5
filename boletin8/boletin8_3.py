13c4ca840c054c5c56cd11b96fe8d3bba577c449
Gabriela <gmartinezmenegatty@danielcastelao.org>
2025-10-15 20:41:28 +0200

    boletin8_3

---
+++ boletin8/boletin8_3.py
@@ -0,0 +1,8 @@
+#Escribir unha función que reciba unha tupla con nomes e para cada nome 
+# imprima unha mensaxe ‘Estimado don/dona Nome’
+
+def saludo():
+    nome = input("Introduce un nombre: ")
+    print(f"Estimado don/dona {nome}")
+
+saludo()
